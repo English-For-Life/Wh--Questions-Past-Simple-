3486a34b20f6e5ccd4b3a3049d092cfcac29c08e
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-07-10 12:57:48 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,82 @@
+//Array of objects
+const quiz = [
+
+    {
+        q:" … did you do yesterday? - I went shopping.",
+        options:["What", "Who", "Where"],
+        answer: 0
+    },
+    {
+        q:" … did you get to New York? - We traveled by bus.",
+        options:["Who", "How", "Where"],
+        answer: 1
+    },
+    {
+        q:" … old were you last year? - I was twenty-three.",
+        options:["Why", "What", "How"],
+        answer: 2
+    },
+    {
+        q:" … was that noise? - It was just the wind.",
+        options:["What", "Who", "Where"],
+        answer: 0
+    },
+    {
+        q:" … were they? - They were at the library.",
+        options:["What", "Where", "When"],
+        answer: 1
+    },
+    {
+        q:" … did you have lunch? - We had lunch at noon.",
+        options:["How", "What", "When"],
+        answer: 2
+    },
+    {
+        q:" … was your teacher? - Ms. Jones was my teacher.",
+        options:["Who", "Why", "What"],
+        answer: 0
+    },
+    {
+        q:" … were you late? - I missed the bus.",
+        options:["Who", "Why", "When"],
+        answer: 1
+    },
+    {
+        q:" … was that on the phone. - It was Steven.",
+        options:["Where", "Why", "Who"],
+        answer: 2
+    },
+    {
+        q:" … did she say? - She said, 'Hello'.",
+        options:["What", "When", "Who"],
+        answer: 0
+    },
+    {
+        q:" … did they graduate? - They graduated in 2014.",
+        options:["What", "When", "Who"],
+        answer: 1
+    },
+    {
+        q:" … time did you go to bed? - I went to bed at eleven o’clock.",
+        options:["How", "When", "What"],
+        answer: 2
+    },
+    {
+        q:" … did he see a doctor? - He hurt his hand.",
+        options:["Why", "Who", "Where"],
+        answer: 0
+    },
+    {
+        q:" … were your bags? - They were on the train.",
+        options:["Why", "Where", "How"],
+        answer: 1
+    },
+    {
+        q:" … was the movie? - It was great!",
+        options:["Why", "Where", "How"],
+        answer: 2
+    }
+
+
+
+]
